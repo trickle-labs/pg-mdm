3242d30c8231acfd15a8e7f6c051a81cbc4dcb0d
Geir Ove Grønmo <grove@geirove.org>
2026-09-12 10:41:52 +0200

    Grant helper execution on normalization functions

--- sql/configure_helper.sql
+++ sql/configure_helper.sql
@@ -36,6 +36,8 @@ GRANT EXECUTE ON FUNCTION pgtrickle.graph_contract(regclass[]) TO :"helper_owner
 GRANT EXECUTE ON FUNCTION pgtrickle.refresh_graph_strict(regclass[], bytea, text) TO :"helper_owner";
 GRANT EXECUTE ON FUNCTION pgtrickle.encode_row_id_v2(text, anyelement) TO :"helper_owner" WITH GRANT OPTION;
 GRANT EXECUTE ON FUNCTION pgtrickle.drop_stream_table(text, boolean) TO :"helper_owner";
+GRANT EXECUTE ON FUNCTION mdm_internal.normalize_text(text, text, integer, text, jsonb) TO :"helper_owner";
+GRANT EXECUTE ON FUNCTION mdm_internal.normalize_date(date, text, integer, text, jsonb) TO :"helper_owner";
 ALTER TABLE mdm_internal.operations OWNER TO :"helper_owner";
 ALTER TABLE mdm_internal.entities OWNER TO :"helper_owner";
 ALTER TABLE mdm_internal.definitions OWNER TO :"helper_owner";
