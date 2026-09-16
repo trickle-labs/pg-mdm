450192bed4acc81d08b382f66ee12ceb7570bc55
Geir Ove Grønmo <grove@geirove.org>
2026-09-16 22:55:22 +0200

    Fix v0.13 package archive

--- sql/archive/pg_mdm--0.13.0.sql
+++ sql/archive/pg_mdm--0.13.0.sql
@@ -700,7 +700,7 @@ CREATE FUNCTION mdm_internal.persist_recompile(request internal) RETURNS jsonb S
 /* </end connected objects> */
 
 /* <begin connected objects> */
--- src/api/refresh.rs:2317
+-- src/api/refresh.rs:2318
 -- pg_mdm::api::refresh::persist_refresh
 CREATE FUNCTION mdm_internal.persist_refresh(request internal) RETURNS jsonb SECURITY DEFINER SET search_path TO pg_catalog, mdm_internal, pg_temp LANGUAGE c AS 'MODULE_PATHNAME', 'persist_refresh_wrapper';
 /* </end connected objects> */
@@ -712,13 +712,13 @@ CREATE FUNCTION mdm_internal.prepare_rebind(request internal) RETURNS jsonb SECU
 /* </end connected objects> */
 
 /* <begin connected objects> */
--- src/api/refresh.rs:3048
+-- src/api/refresh.rs:3049
 -- pg_mdm::api::refresh::preview_entity
 CREATE FUNCTION mdm_internal.preview_entity(request internal) RETURNS jsonb SECURITY DEFINER SET search_path TO pg_catalog, mdm_internal, pg_temp LANGUAGE c AS 'MODULE_PATHNAME', 'preview_entity_wrapper';
 /* </end connected objects> */
 
 /* <begin connected objects> */
--- src/api/refresh.rs:2342
+-- src/api/refresh.rs:2343
 -- pg_mdm::api::refresh::preview
 CREATE FUNCTION mdm.preview(entity_name text, mode text DEFAULT 'validation', options jsonb DEFAULT '{}'::jsonb) RETURNS jsonb LANGUAGE c AS 'MODULE_PATHNAME', 'preview_wrapper';
 /* </end connected objects> */
@@ -730,7 +730,7 @@ CREATE FUNCTION mdm_admin.rebind(entity_name text) RETURNS jsonb LANGUAGE c AS '
 /* </end connected objects> */
 
 /* <begin connected objects> */
--- src/api/refresh.rs:2301
+-- src/api/refresh.rs:2302
 -- pg_mdm::api::refresh::rebuild
 CREATE FUNCTION mdm_admin.rebuild(entity_name text, full_policy text DEFAULT 'ALLOW') RETURNS jsonb LANGUAGE c AS 'MODULE_PATHNAME', 'rebuild_wrapper';
 /* </end connected objects> */
@@ -742,7 +742,7 @@ CREATE FUNCTION mdm_admin.recompile(entity_name text) RETURNS jsonb LANGUAGE c A
 /* </end connected objects> */
 
 /* <begin connected objects> */
--- src/api/refresh.rs:2285
+-- src/api/refresh.rs:2286
 -- pg_mdm::api::refresh::refresh
 CREATE FUNCTION mdm.refresh(entity_name text, full_policy text DEFAULT 'ALLOW') RETURNS jsonb LANGUAGE c AS 'MODULE_PATHNAME', 'refresh_wrapper';
 /* </end connected objects> */
